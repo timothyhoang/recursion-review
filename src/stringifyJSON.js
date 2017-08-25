d508618c7031bdc604537c581d8a3380f1d7afe8
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-08-25 01:02:44 -0700

    Implemented stringifyJSON and fixed spec test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,52 @@
 
 // but you don't so you're going to write it from scratch:
 
+var stringifyElement = function(element, hasWrapper) {
+  if (typeof element === 'undefined' || typeof element === 'function') {
+    if (hasWrapper) {
+      return 'null';
+    } else {
+      return undefined;
+    }
+  } else if (typeof element === 'string') {
+    if (hasWrapper) {
+      return element;
+    } else {
+      return '"' + element + '"';
+    }
+  } else {
+    return '' + element;
+  }
+};
+
+var stringifyArray = function(arr) {
+  var elements = [];
+  for (let element of arr) {
+    elements.push(stringifyJSON(element));
+  }
+  return '[' + elements.join(',') + ']';
+};
+
+var stringifyObject = function(obj) {
+  var elements = [];
+  for (var key in obj) {
+    if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+      elements.push('"' + key + '":' + stringifyJSON(obj[key]));
+    }
+  }
+  return '{' + elements.join(',') + '}';
+}
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringifyJSON = function(obj, hasWrapper) {
+    if (Array.isArray(obj)) {
+      return stringifyArray(obj, true);
+    } else if (typeof obj === 'object' && obj !== null) {
+      return stringifyObject(obj, true);
+    } else {
+      return stringifyElement(obj, hasWrapper);
+    }  
+  }
+
+  return stringifyJSON(obj);
 };
